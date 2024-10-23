0f5cde696a2fe38c2e039306c272ea59a8a00e16
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-10-23 21:53:38 +0100

    Fix SyntaxError by using a valid parameter name in square function

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -3,8 +3,14 @@
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+/* function square(3) {
+    return num * num;
+} */
+
+function square(num) {
     return num * num;
 }
 
+// Uncaught SyntaxError: Unexpected number
 
+// The parameter of a function cannot be a value like 3. Instead, it should be a valid identifier 
