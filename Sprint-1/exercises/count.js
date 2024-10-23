d86093a576dbcbf25965af893295c2740a4e3d82
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-10-23 16:31:54 +0100

    Increment count variable by 1

--- Sprint-1/exercises/count.js
+++ Sprint-1/exercises/count.js
@@ -4,3 +4,5 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+//Description: Line 3 retrieves the current value of count, adds 1 to it, and then assigns the result back to count. The = operator updates count with the new value after the addition.
