d633f6e37f25d226893631b86458380133fda7c3
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-10-23 21:43:14 +0100

    Fix redeclaration error in capitalise function by removing 'let' keyword

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -4,6 +4,12 @@
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
+
+console.log(capitalise("danial"));
+
+
+
+// The error occurred due to redeclaring the parameter str with let. The fix was to update str directly without redeclaring it.
