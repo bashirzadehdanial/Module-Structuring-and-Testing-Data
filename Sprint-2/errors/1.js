a9c5318d8c22db560a2a4cc9aa708b9e0b7056f3
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-10-23 21:51:14 +0100

    Fix redeclaration error in convertToPercentage function and correct console log usage

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -4,10 +4,23 @@
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
+  console.log(decimalNumber);
 
   return percentage;
 }
 
-console.log(decimalNumber);
+convertToPercentage(2.55)
+
+
+
+// Uncaught SyntaxError: Identifier 'decimalNumber' has already been declared
+// To fix this, I should remove the redeclaration
+
+// The second issue is: ReferenceError: decimalNumber is not defined
+/*
+The console.log(decimalNumber); statement outside the function tries to log the decimalNumber variable, which is not defined in that scope. 
+This leads to a ReferenceError indicating that decimalNumber is not recognised outside the function. 
+ */ 
+// // To fix this, I should remove the log or put it in the scop.
