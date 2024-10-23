5c5f7df23675beeb65fdd6b4de3511e8283df8a9
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-10-23 21:31:12 +0100

    Fix sum function to correctly return the sum of two numbers

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,11 @@
 // Predict and explain first...
 
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// It has a return statement immediately followed by a semicolon. As a result, the function will terminate at the return statement without executing the subsequent line, which means it does not return a value.
+// The sum(10, 32) function call will execute first, but since the function does not return a value, it returns undefined. 
+//The output will be: console.log("The sum of 10 and 32 is undefined");
