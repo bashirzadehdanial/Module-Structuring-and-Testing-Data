c8ae532f7078a0e22fa0e6215fd2a50784398999
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-10-23 14:52:45 +0100

    Fix TypeError by converting cardNumber to string before extracting last 4 digits

--- Sprint-1/errors/3.js
+++ Sprint-1/errors/3.js
@@ -1,5 +1,8 @@
 const cardNumber = 4533787178994213;
-const last4Digits = cardNumber.slice(-4);
+const last4Digits = cardNumber.toString().slice(-4);
+console.log(last4Digits);
+
+
 
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
@@ -7,3 +10,7 @@ const last4Digits = cardNumber.slice(-4);
 // Then run the code and see what error it gives.
 // Consider: Why does it give this error? Is this what I predicted? If not, what's different?
 // Then try updating the expression last4Digits is assigned to, in order to get the correct value
+
+ /*Solution: The slice() method only works on strings, not numbers.
+ To fix the issue, convert cardNumber to a string using toString() before applying slice().
+ This allows us to extract the last 4 digits correctly.*/
