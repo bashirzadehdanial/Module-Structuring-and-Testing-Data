105d6ee16882ea442f1a1202649b2c5da12b2929
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-10-23 21:26:25 +0100

    Explain behavior of multiply function and console output

--- Sprint-2/debug/0.js
+++ Sprint-2/debug/0.js
@@ -5,3 +5,5 @@ function multiply(a, b) {
 }
 
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+
+//The code logs 320 from multiply(10, 32), because it’s inside the template literal ${}, but the final output is The result of multiplying 10 and 32 is undefined because the function doesn't return a value, only logs it.
