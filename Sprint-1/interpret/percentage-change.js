39c8cb2047f434b4f191fe324cfb2d1d58025de9
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-10-23 19:20:41 +0100

    fix function errors, and explain expressions

--- Sprint-1/interpret/percentage-change.js
+++ Sprint-1/interpret/percentage-change.js
@@ -2,7 +2,7 @@ let carPrice = "10,000";
 let priceAfterOneYear = "8,543";
 
 carPrice = Number(carPrice.replaceAll(",", ""));
-priceAfterOneYear = Number(priceAfterOneYear.replaceAll("," ""));
+priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
 
 const priceDifference = carPrice - priceAfterOneYear;
 const percentageChange = (priceDifference / carPrice) * 100;
@@ -13,10 +13,25 @@ console.log(`The percentage change is ${percentageChange}`);
 
 // a) How many function calls are there in this file? Write down all the lines where a function call is made
 
+/*Line 4: carPrice.replaceAll(",", "")
+Line 5: priceAfterOneYear.replaceAll(",", "")
+Line 4: Number(...) (for carPrice)
+Line 5: Number(...) (for priceAfterOneYear)
+Line 10: console.log(...)*/
+
 // b) Run the code and identify the line where the error is coming from - why is this error occurring? How can you fix this problem?
+
+// There is a syntax error due to a missing comma in the replaceAll method call. To fix it, we can add a comma between the parameters: priceAfterOneYear = Number(priceAfterOneYear.replaceAll(",", ""));
+
 
 // c) Identify all the lines that are variable reassignment statements
 
+// Line 4 and 5
+
 // d) Identify all the lines that are variable declarations
 
+// Line 1, 2, 7, and 8
+
 // e) Describe what the expression Number(carPrice.replaceAll(",","")) is doing - what is the purpose of this expression?
+
+// Convert a string representation of a price with commas into a numerical value for calculations. (replacing commoas with empty space)
