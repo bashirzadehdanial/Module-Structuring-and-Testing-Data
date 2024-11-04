8dcc3907441b4f53a81ac948be374f711a4b4567
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-11-04 10:24:05 +0000

    implement toPounds function for currency conversion to be reusable opposite of the previous method in sprint1

--- Sprint-2/implement/to-pounds.js
+++ Sprint-2/implement/to-pounds.js
@@ -4,3 +4,29 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+
+
+function toPounds(penceInput) {
+
+
+    const penceStringWithoutTrailingP = penceInput.substring(
+      0,
+      penceInput.length - 1
+    );
+    const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+    const pounds = paddedPenceNumberString.substring(
+        0,
+        paddedPenceNumberString.length - 2
+    );
+    const pence = paddedPenceNumberString
+        .substring(paddedPenceNumberString.length - 2)
+        .padEnd(2, "0");
+    return `£${pounds}.${pence}`;
+}
+
+
+console.log(toPounds("399p")); 
+console.log(toPounds("10p"));  
+console.log(toPounds("1p"));   
+console.log(toPounds("500p")); 
+console.log(toPounds("1000p")); 
