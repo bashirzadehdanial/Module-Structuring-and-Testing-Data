6781a14bbcdcfc0bd4ec5de6e4bb63eb772048fa
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-11-04 10:48:47 +0000

    Answer questions regarding formatTimeDisplay function

--- Sprint-2/interpret/time-format.js
+++ Sprint-2/interpret/time-format.js
@@ -19,13 +19,18 @@ function formatTimeDisplay(seconds) {
 // Questions
 
 // a) When formatTimeDisplay is called how many times will pad be called?
-
+// Three times including totalHours,remainingMinutes, and remainingSeconds 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
-
+// Value is 0, as this relate to totalHours
 // c) What is the return value of pad is called for the first time?
-
+// 00
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
+/* 
+When pad is called for the last time, the value assigned to num is 1. 
+This corresponds to remainingSeconds of 61 % 60 = 1, which is the remainingSeconds is 1.
 
+*/
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
+// Value is 01, as it pads 0 to be two characters for the output.
