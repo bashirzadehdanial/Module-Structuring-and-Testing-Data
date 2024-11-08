2469246d497ab0d882047dc9edca350eda40cfb0
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-11-08 23:19:12 +0000

    Implement isValidTriangle function to validate triangle side lengths

--- Sprint-3/implement/is-valid-triangle.js
+++ Sprint-3/implement/is-valid-triangle.js
@@ -33,3 +33,22 @@
 // Then it should return true because the input forms a valid triangle.
 
 // This specification outlines the behavior of the isValidTriangle function for different input scenarios, ensuring it properly checks for invalid side lengths and whether they form a valid triangle according to the Triangle Inequality Theorem.
+
+
+function isValidTriangle(a, b, c) {
+  
+  if (a <= 0 || b <= 0 || c <= 0) {
+    return false; 
+  }
+
+  
+  if (a + b <= c || a + c <= b || b + c <= a) {
+    return false; 
+  }
+
+  return true;
+}
+
+console.log(isValidTriangle(3, 3, 3))
+console.log(isValidTriangle(-1, 2, 3))
+console.log(isValidTriangle(0, 3, 4));
