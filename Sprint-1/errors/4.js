120dda1a40abec4af83936d53d1e089ddda4b17a
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-11-08 20:43:04 +0000

    Use twelveHourFormatTime and twentyFourHourFormatTime to indicate time formats

--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,5 +1,5 @@
-const twelveHourClockTime = "20:53"; // Renamed to start with a letter, not a number
-const twentyhourClockTime = "08:53";
-console.log(twelveHourClockTime);
-console.log(twentyhourClockTime);
+const twelveHourFormatTime = "20:53"; // Renamed to start with a letter, not a number
+const twentyFourHourFormatTime = "08:53";
+console.log(twelveHourFormatTime);
+console.log(twentyFourHourFormatTime);
 
