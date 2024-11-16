edaf61e26e1ae1f53ad9450c6c68b639bd3e19fc
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-11-16 14:44:54 +0000

    Add test for Function should return false if denominator is equal to numerator

--- Sprint-3/implement/is-proper-fraction.test.js
+++ Sprint-3/implement/is-proper-fraction.test.js
@@ -4,5 +4,6 @@ test("Function should throw an error when the denominator is zero, as it's not a
   expect(() => isProperFraction(3, 0)).toThrow("Denominator cannot be zero");
 });
 
-
-
+test("Function should return false if denominator is equal to numerator, because it is not a proper fraction", () => {
+  expect(isProperFraction(3, 3)).toBe(false);
+});
