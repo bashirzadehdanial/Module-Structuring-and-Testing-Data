fc20b6590cefd98015020bc9c26d897a8b80b02e
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-11-16 11:34:26 +0000

    updated

--- Sprint-3/implement/get-angle-type.js
+++ Sprint-3/implement/get-angle-type.js
@@ -25,3 +25,29 @@
 // Identify Reflex Angles:
 // When the angle is greater than 180 degrees and less than 360 degrees,
 // Then the function should return "Reflex angle"
+
+
+function getAngleType(angle) {
+  if (angle === 90) {
+    return "Right angle";
+  } else if (angle < 90) {
+    return "Acute angle";
+  } else if (angle > 90 && angle < 180) {
+    return "Obtuse angle";
+  } else if (angle === 180) {
+    return "Straight angle";
+  } else if (angle > 180 && angle < 360) {
+    return "Reflex angle";
+  } else {
+     return "Invalid angle"; 
+  }
+
+}
+
+module.exports = {getAngleType};
+
+
+
+
+
+
--- Sprint-3/implement/get-card-value.js
+++ Sprint-3/implement/get-card-value.js
@@ -29,3 +29,4 @@
 // Given a card with an invalid rank (neither a number nor a recognized face card),
 // When the function is called with such a card,
 // Then it should throw an error indicating "Invalid card rank."
+
