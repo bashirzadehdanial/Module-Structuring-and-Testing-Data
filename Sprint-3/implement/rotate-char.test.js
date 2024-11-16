a1bbd0c5c1623e1df62374af1cb51c039608c0c3
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-11-16 15:18:18 +0000

    Add test for rotateCharacter function as it should rotate the character by shift positions within the alphabet while handling the wraparound

--- Sprint-3/implement/rotate-char.test.js
+++ Sprint-3/implement/rotate-char.test.js
@@ -14,6 +14,11 @@ test("It should return the character unchanged as the given character is not a l
   expect(rotateCharacter("7", 5)).toBe("7");
 });
 
+test("It should rotate the character by shift positions within the alphabet while handling the wraparound", () => {
+  expect(rotateCharacter("z", 1)).toBe("a");
+  expect(rotateCharacter("Y", 2)).toBe("A");
+});
+
 
 
 
