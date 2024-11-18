de255d8a8f0c352467cd77dc452b5021d99e520c
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-11-18 18:09:34 +0000

    Add test for Function should throw an error when the denominator is zero

---
+++ Sprint-3/implement/is-proper-fraction.test.js
@@ -0,0 +1,17 @@
+const { isProperFraction } = require("./is-proper-fraction");
+
+test("Function should throw an error when the denominator is zero, as it's not a valid fraction", () => {
+  expect(() => isProperFraction(3, 0)).toThrow("Denominator cannot be zero");
+});
+
+test("Function should return false if denominator is equal to numerator, because it is not a proper fraction", () => {
+  expect(isProperFraction(3, 3)).toBe(false);
+});
+
+test("Function should return true if numerator is smaller than denumerator, because it is a proper fraction", () => {
+  expect(isProperFraction(2, 3)).toBe(true);
+});
+
+test("Function should return true if numerator is greater than denumerator, because it is a proper fraction", () => {
+  expect(isProperFraction(4, 3)).toBe(false);
+});
