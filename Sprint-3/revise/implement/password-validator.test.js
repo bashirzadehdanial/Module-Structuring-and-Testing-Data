f66b3fc2e639f2078c9df63a646170ad5ce1894c
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-11-18 17:16:59 +0000

    Implement password validation function with test

--- Sprint-3/revise/implement/password-validator.test.js
+++ Sprint-3/revise/implement/password-validator.test.js
@@ -14,3 +14,63 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
+
+function isValidPassword(password, previousPasswords) {
+  if (password.length < 5) {
+    return false;
+  }
+
+  if (!/[A-Z]/.test(password)) {
+    return false;
+  }
+
+  if (!/[a-z]/.test(password)) {
+    return false;
+  }
+
+  if (!/\d/.test(password)) {
+    return false;
+  }
+
+  if (!/[!#$%.&*]/.test(password)) {
+    return false;
+  }
+
+  if (previousPasswords.includes(password)) {
+    return false;
+  }
+
+  return true;
+}
+
+describe("isValidPassword", () => {
+  const previousPasswords = ["Danial123?", "helloWorld1", "Test@123"];
+
+  test("should return false if password is less than 5 characters", () => {
+    expect(isValidPassword("1234", previousPasswords)).toBe(false);
+  });
+
+  test("should return false if password has no uppercase letter", () => {
+    expect(isValidPassword("danial123?", previousPasswords)).toBe(false);
+  });
+
+  test("should return false if password has no lowercase letter", () => {
+    expect(isValidPassword("DANIAL123?", previousPasswords)).toBe(false);
+  });
+
+  test("should return false if password has no digit", () => {
+    expect(isValidPassword("Danial?", previousPasswords)).toBe(false);
+  });
+
+  test("should return false if password has no special symbol", () => {
+    expect(isValidPassword("Danial123", previousPasswords)).toBe(false);
+  });
+
+  test("should return false if password is a previous password", () => {
+    expect(isValidPassword("Danial123?", previousPasswords)).toBe(false);
+  });
+
+  test("should return true if password meets all requirements", () => {
+    expect(isValidPassword("Danial123!", previousPasswords)).toBe(true);
+  });
+});
